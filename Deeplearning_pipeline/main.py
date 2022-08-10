4464ca2f1cbd994a18a17a9a7e1416df6f27de54
SathwikSai10 <sathwik.nagasai@gmal.com>
2022-08-10 16:18:09 +0200

    Add todo comments to the script

--- Deeplearning_pipeline/main.py
+++ Deeplearning_pipeline/main.py
@@ -6,17 +6,13 @@ from data import load_data
 import neptune.new as neptune
 from torchvision.models import resnet18
 from train import train_model
-from helpers import save_architecture_txt, one_hot_embedding
-
-
+from helpers import save_architecture_txt
 from losses import mse_loss,edl_loss,edl_mse_loss
 
 # Initialing the neptune logger.
 run = neptune.init('Provide your neptune ai key')
 
 # Set the dataset path for train data and saving the results
-
-
 data_dir = 'set_path/training_dataset'
 save_path = 'set_path/to_save_results'
 
@@ -28,10 +24,9 @@ parameters = { 'num_epochs': 40,
                 'lr': 1e-3,
                 'weight_decay':1e-5,
                 'device': torch.device("cuda:0" if torch.cuda.is_available() else "cpu")}
-
+                
 # Logging hyperparameters.
 run['config/hyperparameters'] = parameters
-
 
 # Load the dataloaders
 dataloader,class_names = load_data(data_dir=data_dir,batch_size=parameters['batch_size'],logger=run)
@@ -40,14 +35,14 @@ dataloader,class_names = load_data(data_dir=data_dir,batch_size=parameters['batc
 model = resnet18(weights = 'DEFAULT')
 model.fc = nn.Linear(in_features=512,out_features=parameters['num_classes'])
 
-
+# Uncertainty is required or not.
 uncertainty = True
+
 # Creating a loss function
 if uncertainty:
     loss_function = edl_mse_loss
 else:
     loss_function = nn.CrossEntropyLoss()
-
 
 # Creating an optimizer
 optimizer = torch.optim.Adam(model.parameters(),lr=parameters['lr'],weight_decay = parameters['weight_decay'])
@@ -56,15 +51,12 @@ optimizer = torch.optim.Adam(model.parameters(),lr=parameters['lr'],weight_decay
 lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=5, gamma=0.1)
 # lr_scheduler = torch.optim.lr_scheduler.CosineAnnealingWarmRestarts(optimizer=optimizer,T_0=10,T_mult=2,eta_min=1e-4)
 
-
-
 # Log the model architecture, loss_function and optimizer
 run['config/model'] = type(model).__name__
 run['config/criterion'] = type(loss_function).__name__
 run['config/optimizer'] = type(optimizer).__name__
 
 # Save model architecture as a text file
-
 save_architecture_txt(model=model,dir_path=save_path,filename=parameters['model_name'])
 
 # Training results
@@ -80,3 +72,6 @@ training_results = train_model(model=model,
 
 best_model,train_losses,valid_losses = training_results
 
+# TODO: Add conusion matrix plots, losses plots using seaborn and pandas and save the results 
+# TODO: show_sample_images. 
+# TODO: (future work) add arguments from the commandline as a json file
